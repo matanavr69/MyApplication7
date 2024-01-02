968d46054f5214ed3d8c052dcbcf932ebdffbcb4
Matan Abraham <matanavrham699@gmail.com>
2024-01-02 14:38:52 +0200

    final error

--- app/src/main/java/com/example/myapplication7/MainActivity.java
+++ app/src/main/java/com/example/myapplication7/MainActivity.java
@@ -4,21 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv1;
-
+    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv1 = findViewById(R.id.tv1id);
+        button1 = findViewById(R.id.button1);
     }
 
     public void Click(View view) {
-        tv1.setText("Oh, yea, I’ve been clicked!\n");
-        tv1.setTextSize(20);
+        button1.setText("Oh, yea, I’ve been clicked!");
     }
 }
