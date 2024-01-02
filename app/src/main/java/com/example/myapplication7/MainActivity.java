9c17cc81444ca9f78bed7a9a287840c4b3a786e8
Matan Abraham <matanavrham699@gmail.com>
2024-01-02 14:17:43 +0200

    final commit

--- app/src/main/java/com/example/myapplication7/MainActivity.java
+++ app/src/main/java/com/example/myapplication7/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.myapplication7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = findViewById(R.id.tv1id);
+    }
+
+    public void Click(View view) {
+        tv1.setText("Oh, yea, I’ve been clicked!\n");
     }
 }
